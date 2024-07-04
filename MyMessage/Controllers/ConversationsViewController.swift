805bca3b31c173657974859e90d44c8853862842
Igor Guryan <sacujo93@yandex.ru>
2024-07-04 16:13:47 +0300

    commit

---
+++ MyMessage/Controllers/ConversationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMessage
+//
+//  Created by Igor Guryan on 04.07.2024.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
